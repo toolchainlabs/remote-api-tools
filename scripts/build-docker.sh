f86466b578e03abb456205a036be20991e8e5bb9
Asher Foa <1268088+asherf@users.noreply.github.com>
2021-03-15 09:43:14 -0700

    add comment.

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-set -euo pipefail
-
 cd "$(git rev-parse --show-toplevel)"
 
 docker build . --build-arg APP_NAME=smoketest --tag smoketest
 docker build . --build-arg APP_NAME=casload --tag casload
 
+# sanity check, run apps in containers (they will show help)
 docker run -it --rm smoketest
 docker run -it --rm casload
