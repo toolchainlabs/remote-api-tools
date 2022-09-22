2883cbd271cf2696c84dca2c6f26f9967f8a837a
Tom Dyas <tdyas@toolchain.com>
2022-09-22 16:47:10 -0400

    regen proto files with latest protoc (#69)

--- scripts/gen-protos.sh
+++ scripts/gen-protos.sh
@@ -7,7 +7,8 @@ fi
 
 protoc \
   -Iprotos/ \
-  --go_out=,plugins=grpc:protos/ \
+  --go_out=protos/ \
+  --go-grpc_out=protos/ \
   --go_opt=Mbuild/bazel/semver/semver.proto=github.com/toolchainlabs/remote-api-tools/protos/build/bazel/semver \
   protos/build/bazel/semver/*.proto \
   protos/build/bazel/remote/execution/v2/*.proto
